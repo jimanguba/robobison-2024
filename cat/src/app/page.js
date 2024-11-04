3d14d528d285b83017c1b0e24842ed76e0fb4df4
Ashley Davis Kosasih <ashleydaviskosasih@gmail.com>
2024-11-03 19:51:20 -0600

    Minor fixes

--- cat/src/app/page.js
+++ cat/src/app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import catEars from  './images/DESIGN_catHead-removebg-preview.png';
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Cat app :p
-    </div>
+      <Image src = {catEars} alt = "Cat ears"></Image>
+    </div> 
   );
 }
