acd4729ce37bd3812ba5ee43299dc33cae3ba86d
Ashley Davis Kosasih <ashleydaviskosasih@gmail.com>
2024-11-10 17:09:41 -0600

    Minor changes

--- cat/src/app/page.js
+++ cat/src/app/page.js
@@ -1,15 +1,22 @@
-import Image from "next/image";
-import catFace from  './images/DESIGN_catHead-removebg-preview.png';
-//import catEars from  './images/DESIGN_catEar-removebg-preview.png'; 
+"use client";
 
+import Image from "next/image";
+import catFace from "./images/DESIGN_catHead-removebg-preview.png";
 
 export default function Home() {
   return (
     <div className="gradient-background">
-      <h2 className="landing-page-message">Hello! Welcome to</h2>
-      <Image className = "cat-image" src = {catFace} alt = "Cat face"></Image>
-      <h1 className="landing-page-title">Cat app :p</h1>
-    </div>
+      <h2 className="landing-page-message">Welcome to</h2>
 
+      {/* Cat image button */}
+      <button
+        className="cat-button"
+        onClick={() => alert("Cat button clicked!")} //change the allert to going to the next page (log-in page)
+      >
+        <Image className="cat-image" src={catFace} alt="Cat face" />
+      </button>
+
+      <h1 className="landing-page-title">Pet Care App</h1>
+    </div>
   );
 }
