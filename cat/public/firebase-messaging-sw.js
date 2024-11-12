fcf2c538ded815f145d7392f6911ff9cd9a6e6e1
Jillian Manguba <manguba3@myumanitoba.ca>
2024-11-11 22:03:01 -0600

    minor sw credential fix

--- cat/public/firebase-messaging-sw.js
+++ cat/public/firebase-messaging-sw.js
@@ -5,12 +5,12 @@ importScripts(
 );
 
 firebase.initializeApp({
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyAydaNmVWQ_Je7rTaJVknuNCbBmPBGZ0xQ',
+  authDomain: 'petapp-8a369.firebaseapp.com',
+  projectId: 'petapp-8a369',
+  storageBucket: 'petapp-8a369.firebasestorage.app',
+  messagingSenderId: '386496141516',
+  appId: '1:386496141516:web:df98001a6115d982b00a69',
 });
 
 // Retrieve firebase messaging
