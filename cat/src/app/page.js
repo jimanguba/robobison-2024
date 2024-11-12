0e4f16887771ed9832e2f6149d45b8bac8f19971
Jillian Manguba <manguba3@myumanitoba.ca>
2024-11-11 21:57:08 -0600

    add notification form for testing

--- cat/src/app/page.js
+++ cat/src/app/page.js
@@ -5,6 +5,7 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebaseClient";
 import { useRouter } from "next/navigation";
 import LogOutButton from "./authentication/log-out/logoutButton";
+import AddNotificationForm from "./components/AddNotificationForm";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
@@ -24,6 +25,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Pet app: {user ? user.email : "Dume"}
+      <AddNotificationForm />
       <LogOutButton />
     </div>
   );
